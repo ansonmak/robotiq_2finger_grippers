e05f22525790fd02e8d274b1792f174aac40d95a
ansonmak <ansonmak1997@gmail.com>
2024-01-22 10:00:31 +0800

    Add simple controller

---
+++ robotiq_2f_gripper_control/scripts/gripper_controller.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+import rospy
+
+# Brings in the SimpleActionClient
+import actionlib
+
+from robotiq_2f_gripper_msgs.msg import CommandRobotiqGripperFeedback, CommandRobotiqGripperResult, CommandRobotiqGripperAction, CommandRobotiqGripperGoal
+from robotiq_2f_gripper_control.robotiq_2f_gripper_driver import Robotiq2FingerGripperDriver as Robotiq
+
+rospy.init_node('robotiq_controller')
+
+action_name = rospy.get_param('~action_name', 'command_robotiq_action')
+robotiq_client = actionlib.SimpleActionClient(action_name, CommandRobotiqGripperAction)
+robotiq_client.wait_for_server()
+
+def control_gripper():
+
+    while not rospy.is_shutdown():
+        print("Go to (0-255): ")
+        x = int(input())
+        width = x/255 * 0.085
+        Robotiq.goto(robotiq_client, pos=width, speed=0.1, force=1, block=True)
+
+if __name__ == '__main__':
+    control_gripper()
