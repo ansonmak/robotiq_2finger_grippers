7d655aba1401201a0228adf01df8cc86c08c478e
ansonmak <ansonmak1997@gmail.com>
2024-01-22 10:00:20 +0800

    Correct simulation motion

--- robotiq_2f_gripper_control/scripts/robotiq_2f_action_client_example.py
+++ robotiq_2f_gripper_control/scripts/robotiq_2f_action_client_example.py
@@ -44,10 +44,14 @@ def operate_gripper():
     # Use pre-defined functions for robot gripper manipulation.
     #####################################################################################
     while not rospy.is_shutdown():
-        Robotiq.goto(robotiq_client, pos=0.00, speed=0.1, force=100 , block=True)
-        Robotiq.goto(robotiq_client, pos=0.04, speed=0.01, force=10)
-        Robotiq.goto(robotiq_client, pos=0.011, speed=0.01, force=0 , block=True)
-        Robotiq.goto(robotiq_client, pos=0.08, speed=0.11, force=200 , block=True)
+        Robotiq.close(robotiq_client, block=True)
+        rospy.sleep(1)
+        Robotiq.open(robotiq_client, block=True) 
+        rospy.sleep(1)
+        # Robotiq.goto(robotiq_client, pos=0.00, speed=0.1, force=100 , block=True)
+        # Robotiq.goto(robotiq_client, pos=0.04, speed=0.01, force=10)
+        # Robotiq.goto(robotiq_client, pos=0.011, speed=0.01, force=0 , block=True)
+        # Robotiq.goto(robotiq_client, pos=0.08, speed=0.11, force=200 , block=True)
         # Robotiq.goto(robotiq_client, pos=0.06, speed=0.0, force=0)
         # break
 
